dd3baa067d6d11e989764008a3e70d2bfbb33758
timothy <timothychia.21@ichat.sp.edu.sg>
2021-11-16 17:51:12 +0800

    Add placeholders

---
+++ src/api/model/friend.js
@@ -0,0 +1,9 @@
+// define function LOGIC for db interaction
+const db = require('../../config/dbConfig');
+const cnx = db.getConn();
+
+module.exports = {
+    selectAll: (callback) => {
+        const selectAllQuery = "";
+    },
+};
